566f8122e26e0e5f550e3148e51b09963edb3fcf
wangjiezhe <wangjiezhe@gmail.com>
2025-10-22 13:31:10 +0800

    ✨ feat(server): add gzip compression middleware

--- app/server.py
+++ app/server.py
@@ -2,6 +2,7 @@ from typing import Any, Dict, List
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.responses import JSONResponse, RedirectResponse
 from pydantic import BaseModel
 
@@ -17,6 +18,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.add_middleware(GZipMiddleware, minimum_size=1000)
 
 
 class FeatureRequest(BaseModel):
