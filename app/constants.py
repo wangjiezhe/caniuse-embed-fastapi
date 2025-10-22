703a4facbe7699dc7ecd135c6bc7d7bc30509fc5
wangjiezhe <wangjiezhe@gmail.com>
2025-10-22 13:22:56 +0800

    ✨ feat(constants): update mdn data source url

--- app/constants.py
+++ app/constants.py
@@ -1,4 +1,7 @@
-MDN_DATA_URL: str = "https://unpkg.com/@mdn/browser-compat-data/data.json"
+# MDN_DATA_URL: str = "https://unpkg.com/@mdn/browser-compat-data/data.json"
+MDN_DATA_URL: str = (
+    "https://cdn.jsdelivr.net/npm/@mdn/browser-compat-data@latest/data.json"
+)
 CANIUSE_URL: str = (
     "https://cdn.jsdelivr.net/gh/Fyrd/caniuse@master/fulldata-json/data-2.0.json"
 )
